a21e19f89d1f0da6b1ec71ad58c10c9db8758470
Sylwia Kołoczek <skoloczek1995@gmail.com>
2019-03-22 20:45:37 +0100

    prepared files

---
+++ js/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'scss/partials./_Section1.scss'; // assuming a styles directory under src/
+//@import '~nprogress/nprogress'; // importing a css file from the nprogress node module
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    class Log_Register extends React.Component {
+        render() {
+            let className='a';
+            return (
+                <div>
+                <h1 className={className}>elo</h1>
+                </div>
+            )
+        }
+    }
+
+    class Section1 extends React.Component {
+        render() {
+            return (
+                <div>
+                    <Log_Register/>
+                    {/*<Menu/>,*/}
+                    {/*<Slogan/>,*/}
+                    {/*<Options />*/}
+                </div>
+            )
+        }
+    }
+
+    const App = () => <Section1/>
+
+    ReactDOM.render(
+        <App/>,
+        document.querySelector('#app')
+    )
+});
