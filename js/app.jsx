40c2a9ccc08272388475764d7658d23b368ec667
Sylwia Kołoczek <skoloczek1995@gmail.com>
2019-03-29 14:13:43 +0100

    component division added in section5.jsx

--- js/app.jsx
+++ js/app.jsx
@@ -5,7 +5,7 @@ import {Section1, Menu, Slogan_Options} from './section1.jsx'
 import Section2 from './section2.jsx'
 import {Section3, S3_Header, S3_Steps, S3_Button} from './section3.jsx'
 import {Section4, S4_About, S4_Picture} from './section4.jsx'
-import {Section5, S5_Header} from './section5.jsx'
+import {Section5, S5_Header, S5_Intro, S5_Main_Buttons} from './section5.jsx'
 import {Section6, Form, S6_Header, S6_Form_Part, S6_Form_Info, S6_Button, Social_Media} from './section6.jsx'
 
 class App extends React.Component {
--- js/section5.jsx
+++ js/section5.jsx
@@ -2,19 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./../scss/main.scss";
 
-export class S5_Header extends React.Component {
+//zrobić button dynamicznie?
+export class S5_Main_Buttons extends React.Component {
     render() {
         return (
-            <div className={'s5_header'}>
+            <div className={'s5_main_buttons'}>
+                <button>Fundacjom</button>
+                <button>Organizacjom pozarządowym</button>
+                <button>Lokalnym zbiórkom</button>
+            </div>
+        )
+    }
+}
+
+export class S5_Intro extends React.Component {
+    render() {
+        return (
                 <div className={'s5_intro'}>
                     <h1>Komu pomagamy?</h1>
                     <img src={'images/decoration.png'}></img>
                 </div>
-                <div className={'s5_main_buttons'}>
-                    <button>Fundacjom</button>
-                    <button>Organizacjom pozarządowym</button>
-                    <button>Lokalnym zbiórkom</button>
-                </div>
+        )
+    }
+}
+
+export class S5_Header extends React.Component {
+    render() {
+        return (
+            <div className={'s5_header'}>
+                <S5_Intro/>
+                <S5_Main_Buttons/>
             </div>
         )
     }
